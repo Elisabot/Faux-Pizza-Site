92f2e72f778391141017ee643d93bbcc5667907a
Elisabeth Meyer <elisabeth.nimtzmeyer@gmail.com>
2014-09-17 11:13:06 -0700

    Added changes from our first meeting

---
+++ main.js
@@ -0,0 +1,17 @@
+window.onload = Init;
+function Init() {
+	alert("BITCH");
+}
+var $crustType = $('#crust-type');
+$crustType.css( "color", "red" );
+
+var $pizzaCount = $('h2');
+$pizzaCount.css("color", '#458283');
+$pizzaCount.on("click", function (event) {
+	$(this).css('color',"purple")
+});
+
+// Can you please make the h2 purple when someone clicks on it?
+
+// Yo, bitch!Make h2 purple muthafucka!
+
