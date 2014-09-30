0b8a581674a41bda11ace3925faab64179e8ba8a
MelissaCunningham <mdafnos@hotmail.com>
2014-09-29 20:57:59 -0700

    melissa and treasure have fun

--- hide.js
+++ hide.js
@@ -2,8 +2,41 @@
 
   function hide()
   {
-    document.getElementById("div1").style.display = "none";
-    document.getElementById("div2").style.display = "block";
+
+    var size = document.getElementsByName("size");
+    var msg = "";
+    msg = "Current Price: $";
+    if (size[0].checked === true) {
+       price = 12.99;
+       msg = msg + price.toString();
+       document.getElementById("div5").style.display = "block";
+       document.getElementById("div5").innerHTML = msg;
+       document.getElementById("div1").style.display = "none";
+       document.getElementById("div2").style.display = "block";
+    }
+    else if (size[1].checked === true) {
+       price = 14.99;
+       msg = msg + price.toString();
+       document.getElementById("div5").style.display = "block";
+       document.getElementById("div5").innerHTML = msg;
+       document.getElementById("div1").style.display = "none";
+       document.getElementById("div2").style.display = "block";
+    }
+    else if (size[2].checked === true) {
+       price = 16.99;
+       msg = msg + price.toString();
+       document.getElementById("div5").style.display = "block";
+       document.getElementById("div5").innerHTML = msg;
+       document.getElementById("div1").style.display = "none";
+       document.getElementById("div2").style.display = "block";
+    }
+    else {
+       msg = "YO, choose a size biotch!";
+       document.getElementById("div5").style.display = "block";
+       document.getElementById("div5").innerHTML = "<h1 class='error-message'>" + msg + "</h1>";
+    }
+
+
   };
   
     function hide2()
